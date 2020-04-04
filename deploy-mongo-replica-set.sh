0db963238e1d8df152fe78885e2da0ff621221c2
Md. Imadul Islam <aurick88@gmail.com>
2020-04-04 18:36:10 +0600

    docker-compose file added to deploy mongo with replica set in one mechine

---
+++ deploy-mongo-replica-set.sh
@@ -0,0 +1,14 @@
+docker-compose -f docker-compose-replicaset.yml up -d && docker exec -it mongo1 mongo;
+
+# Exec the command
+# rs.initiate({"_id":"my_replica","members":[{"_id":0,"host":"192.168.88.17:27017"},{"_id":1,"host":"192.168.88.17:27018"},{"_id":2,"host":"192.168.88.17:27019"}]})
+
+# For second-rep-set press Ctrl + C
+
+# Connection string
+# mongodb://192.168.88.17:27017,192.168.88.17:27018,192.168.88.17:27019/aurickdb?replicaSet=my_replica
+
+
+# rs.initiate({"_id":"my_replica","members":[{"_id":0,"host":"192.168.88.17:27017"},{"_id":1,"host":"192.168.88.18:27017"}]})
+# Connection string
+# mongodb://192.168.88.17:27017,192.168.88.18:27017/aurickdb?replicaSet=my_replica
